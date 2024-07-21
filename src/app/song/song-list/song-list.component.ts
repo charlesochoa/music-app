122b01cda728e1e57fdda44a276da8f0f2fe82ba
Charles Ochoa <57524710+charlesochoa@users.noreply.github.com>
2024-07-21 14:38:29 +0200

    Merge pull request #3 from charlesochoa/feature/visual-structure-song-list
    Layout song list

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, ToolbarComponent],
+  imports: [
+    MatCardModule,
+    ToolbarComponent,
+    MatGridListModule,
+    MatIconModule,
+    CommonModule,
+    NgFor,
+  ],
   templateUrl: './song-list.component.html',
   styleUrl: './song-list.component.scss',
 })
-export class SongListComponent {}
+export class SongListComponent {
+  // TODO: extract data calling the json-server
+  songs = [
+    {
+      id: 1,
+      title: 'Who did you think i was',
+      poster: 'http://dummyimage.com/400x600.png/cc0000/ffffff',
+      genre: ['Pop', 'Rock', 'Alternative'],
+      year: 2007,
+      duration: 424,
+      rating: 9.27,
+      artist: 1,
+    },
+    {
+      id: 2,
+      title: 'Penny Lane',
+      poster: 'http://dummyimage.com/400x600.png/dddddd/000000',
+      genre: ['Rock', 'Pop', 'Chill'],
+      year: 1967,
+      duration: 303,
+      rating: 8.99,
+      artist: 2,
+    },
+    {
+      id: 3,
+      title: 'Hey Jude',
+      poster: 'http://dummyimage.com/400x600.png/5fa2dd/ffffff',
+      genre: ['Pop', 'Rock', 'Alternative'],
+      year: 1974,
+      duration: 206,
+      rating: 6.25,
+      artist: 2,
+    },
+    {
+      id: 4,
+      title: "(I Can't Get No) Satisfaction",
+      poster: 'http://dummyimage.com/400x600.png/cc0000/ffffff',
+      genre: ['Rock', 'Pop', 'Heavy'],
+      year: 1965,
+      duration: 290,
+      rating: 6.66,
+      artist: 4,
+    },
+  ];
+}
